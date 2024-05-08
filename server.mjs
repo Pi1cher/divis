83dd2deda744fa774fb5883512305a60f878ea13
Pi1cher <skete2002@gmail.com>
2024-05-08 16:17:33 +0300

    PORT .env

--- server.mjs
+++ server.mjs
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const PORT = process.env.PORT || 3001; // Используем порт из переменной окружения PORT, либо порт по умолчанию 3001
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -17,6 +18,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3001, () => {
-    console.log('Сервер запущен на порту 3001');
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
